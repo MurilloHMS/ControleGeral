2bf4e1601f1a4b2f9dc52fd2503ebbe6ede28662
MurilloHMS <Murillohenriquemelo@outlook.com>
2024-08-02 13:17:56 -0300

    criado enumeradores

---
+++ Model/Enums/StatusCadastros.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KhoraControl.Model.Enums
+{
+    public enum StatusCadastros
+    {
+        CRIADO =0,
+        ATUALIZADO,
+        DELETADO,
+        ENCONTRADO,
+        NAO_ENCONTRADO,
+        ATIVO,
+        DESATIVADO
+    }
+}
---
+++ Model/Enums/StatusImportacao_e.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KhoraControl.Model.Enums
+{
+    public enum StatusImportacao_e
+    {
+        IMPORTADO = 0,
+        NAO_IMPORTADO
+    }
+}
